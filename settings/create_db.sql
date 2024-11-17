3d855237fb5e39d900d6cc77cf361d80acae7d29
IvanPersonal_Prueba1 <148810641+IvanPersonalPrueba1@users.noreply.github.com>
2024-11-17 17:12:25 -0300

    Revert "creado de carpetas"

--- settings/create_db.sql
+++
@@ -1,4 +0,0 @@
--- Crear base de datos
-DROP DATABASE IF EXISTS gestion_inventario;
-CREATE DATABASE gestion_inventario;
-USE gestion_inventario;
--- settings/create_user.sql
+++
@@ -1,8 +0,0 @@
--- Crear usuario para la aplicación
-CREATE USER IF NOT EXISTS 'app_user'@'localhost' IDENTIFIED BY 'secure_password';
-
--- Dar permisos al usuario sobre la base de datos
-GRANT ALL PRIVILEGES ON gestion_inventario.* TO 'app_user'@'localhost';
-
--- Aplicar cambios
-FLUSH PRIVILEGES;
